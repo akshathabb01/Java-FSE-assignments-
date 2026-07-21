00ecb8e39d9fb2ef2751064c8e14828cc81a56d6
akshathabb01 <akshatha.bb01@gmail.com>
2026-07-21 22:04:04 +0530

    Week 3 - Spring Core HandsOn 4 completed

--- Week 3/HandsOn1_CreateSpringWebProject/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week 3/HandsOn1_CreateSpringWebProject/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -2,8 +2,9 @@ package com.cognizant.spring_learn;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
@@ -13,9 +14,25 @@ public class SpringLearnApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringLearnApplication.class, args);
-
         LOGGER.info("Inside main");
+
+        displayCountry();
+
+    }
+
+    public static void displayCountry() {
+
+        LOGGER.info("Start");
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+
+        LOGGER.info("End");
 
     }
 }
